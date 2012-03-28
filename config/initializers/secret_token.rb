60b2898945deef38ab406caf59dc3c770e88ad6a
withgod <noname@withgod.jp>
2012-03-28 15:32:58 +0900

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmNjKanshaWeb::Application.config.secret_token = 'a09b3302bd63690ccbe58941f860ff34791d454eb862205bf4c8a5308276951593abcecf7928e954192636780ee837fa8628d3e99444e39284ef4299a2416dc7'
